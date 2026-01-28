64a0896db7cda27f4a2ff595a2f317e2e04f38d3
Mubasir A. <70850923+MamunOp@users.noreply.github.com>
2026-01-28 08:00:58 +0530

    Create presets.js

---
+++ assets/js/presets.js
@@ -0,0 +1,16 @@
+const EXAM_PRESETS = {
+    neet: {
+        id: "neet",
+        name: "NEET UG 2026",
+        widthMm: 35,
+        heightMm: 45,
+        maxKb: 200,
+    },
+    ssc: {
+        id: "ssc",
+        name: "SSC CGL / CHSL",
+        widthMm: 35,
+        heightMm: 45,
+        maxKb: 50,
+    }
+};
