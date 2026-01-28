6e517c63ea21ef17a8aab83fd77931f315f500ca
Mubasir A. <70850923+MamunOp@users.noreply.github.com>
2026-01-28 06:38:44 +0530

    Create presets.js

---
+++ assets/js/presets.js
@@ -0,0 +1,20 @@
+const presets = {
+    "neet": {
+        id: "neet",
+        name: "NEET UG 2024",
+        widthMm: 35,
+        heightMm: 45,
+        maxKb: 200,
+        minKb: 10,
+        guidelines: ["White background", "80% face coverage", "No glasses"]
+    },
+    "indian-passport": {
+        id: "indian-passport",
+        name: "Indian Passport",
+        widthMm: 51,
+        heightMm: 51,
+        maxKb: 300,
+        guidelines: ["Square 2x2 inch", "White background", "Full face visible"]
+    }
+    // Add more here...
+};
